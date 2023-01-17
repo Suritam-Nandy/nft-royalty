874cb28725e9e74945a1584b1cc4a8bdff11d106
Suritam-Nandy <13suritsuritam@gmail.com>
2023-01-18 00:42:06 +0530

    ✨ add tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      gray: "#62656C",
+      grayLight: "#D9D9D9",
+    },
     extend: {},
   },
   plugins: [],
