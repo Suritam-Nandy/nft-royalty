f239e8064baeecd1f66a8e9d3e6a9443518655b7
Suritam-Nandy <13suritsuritam@gmail.com>
2023-01-18 14:07:58 +0530

    ✨

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import Signup from "./components/pages/Signup";
-import Home from "./components/pages/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import Signup from "./components/pages/Signup";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </BrowserRouter>
   );
