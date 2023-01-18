69cb983d67e0a9920f3d98c2948c9eddcc52fb2c
Suritam-Nandy <13suritsuritam@gmail.com>
2023-01-18 11:18:50 +0530

    🐛

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Routes, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Signup from "./components/pages/Signup";
 import Home from "./components/pages/Home";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Routes>
       <Route exact path="/" component={Home} />
 
       <Route exact path="/signup" component={Signup} />
-    </BrowserRouter>
+    </Routes>
   );
 }
 
