1bb701e1b6eddb48b1e442ca2e6ef4736416b35a
Suritam-Nandy <13suritsuritam@gmail.com>
2023-01-21 13:22:02 +0530

    ⚡️ 🔥

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+import Login from "./components/pages/Login";
 import Signup from "./components/pages/Signup";
 import Home from "./components/pages/Home";
-import Login from "./components/pages/Login";
+import AddCollectionDetails from "./components/pages/AddCollectionDetails";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/addcollectiondetails"
+          component={AddCollectionDetails}
+        />
+
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/login" component={Login} />
       </Switch>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Sidebar from "../layout/Sidebar";
-import Submit from "../layout/Submit";
+import Button from "../layout/Button";
 
 const Home = () => {
   return (
@@ -8,26 +8,26 @@ const Home = () => {
       <div className="flex flex-row min-h-screen bg-gray-100 text-gray-800">
         <Sidebar />
         <main className="main flex flex-col justify-center items-center flex-grow -ml-64 md:ml-0">
-          <div className="main-content flex flex-col justify-center -mt-10 items-center flex-grow p-4">
+          <div className="main-content flex flex-col justify-center -mt-10 items-center flex-grow">
             <div className="flex flex-col justify-center items-center  flex-grow bg-white">
               <div className="max-w-full flex items-center justify-center h-screen m-auto ">
                 <div className="border-solid drop-shadow-xl rounded-lg py-8   px-10 bg-blueBg">
                   <div className=" flex flex-col content-center items-center place-items-evenly ">
-                    <div className="mb-   ">
+                    <div className="">
                       <span className="text-lg font-semibold">
                         There is not currently an NFT collection loaded on the
                         platform. Get started by adding a collection now!
                       </span>
                     </div>
                     <div className="mt-10">
-                      <Submit name={"Add NFT Collection "} />
+                      <Button name={"Add NFT Collection "} />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <footer className="footer px-4 py-6">
+          <footer className="footer px-4 mb-4">
             <div className="footer-content">
               <p className="text-sm text-gray-600 text-center">
                 © Dashboard, All rights reserved
