fc325a5ff33ee624e49801cc967eba6f6f312341
Suritam-Nandy <13suritsuritam@gmail.com>
2023-02-10 13:10:09 +0530

    💄

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -2,10 +2,16 @@ import React from "react";
 import Sidebar from "../layout/Sidebar";
 import Button from "../layout/Button";
 import Footer from "../layout/Footer";
-import AreaChart from "../layout/AreaChart";
-import DoughnutChart from "../layout/DoughnutChart";
+import AreaChart, { data } from "../layout/AreaChart";
+import DoughnutChart, { dataDoughnut } from "../layout/DoughnutChart";
 
 const Dashboard = () => {
+  const legend = {
+    labels: [...dataDoughnut.datasets[0].labels],
+    data: [...dataDoughnut.datasets[0].data],
+    backgroundColor: [...dataDoughnut.datasets[0].backgroundColor],
+  };
+
   const collectionList = [
     {
       icon: "/OpenSea Logo.png",
@@ -121,46 +127,49 @@ const Dashboard = () => {
             <main className="main w-full flex flex-col justify-center items-center flex-grow scrollbar-thin scrollbar-thumb-grayDark scrollbar-track-grayDarkText overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
               <div className="w-full flex flex-col justify-start items-center ">
                 <div className="w-full flex flex-col flex-grow px-8 ">
-                  <div className="w-full flex flex-col font-s justfy-center items-start  my-1">
+                  <div className="w-full flex flex-col font-s justfy-center items-start mb-2.5 mt-">
                     <h1 className="tracking-wide font-medium py-1">
                       Current Pay Period:
                     </h1>
-                    <div className="w-full bg-grayLight rounded-full h-5 dark:bg-grayLight">
+                    <div className="w-full bg-grayLight rounded-full h-4 dark:bg-grayLight">
                       <div
-                        className="bg-grayDarkText   h-5 rounded-full"
+                        className="bg-grayDarkText   h-4 rounded-full"
                         style={{ width: "45%" }}
                       ></div>
                     </div>
                     <h1 className="tracking-wide font-medium ">
                       Days Remaining: 26 days (December 31, 2022)
                     </h1>
+                    <div className="w-full flex flex-row items-start justify-end -mt-4">
+                      <Button name={"Distribute Royalties"} />
+                    </div>
                   </div>
 
-                  <div className="w-full bg-blueBg p-2  drop-shadow-xl rounded-lg ">
+                  <div className="w-full bg-blueBg p-2 py-1  drop-shadow-xl rounded-lg ">
                     <div className="w-full flex flex-row justify-around items-center text-grayDark">
                       <div className="w-max flex flex-col justify-center items-center text-grayText font-bold ">
                         <label>Current Balance</label>
-                        <label className="flex flex-col justify-center items-center text-2xl">
+                        <label className="flex flex-col justify-center items-center text-2xl -mt-1">
                           31.8 Ξ{" "}
-                          <span className="text-sm font-normal text-grayDarkText tracking-wide">
+                          <span className="text-sm font-normal text-grayDarkText tracking-wide -mt-1">
                             $38,160 USD
                           </span>
                         </label>
                       </div>
                       <div className="w-max flex flex-col justify-center items-center text-grayText  font-bold ">
                         <label>Net Income</label>
-                        <label className="flex flex-col justify-center items-center text-2xl">
+                        <label className="flex flex-col justify-center items-center text-2xl -mt-1">
                           21.2 Ξ
-                          <span className="text-sm font-normal text-grayDarkText tracking-wide">
+                          <span className="text-sm font-normal text-grayDarkText tracking-wide -mt-1">
                             $25,440 USD
                           </span>
                         </label>
                       </div>
                       <div className="w-max flex flex-col justify-center items-center text-grayText  font-bold ">
                         <label>Contributor Earnings</label>
-                        <label className="flex flex-col justify-center items-center text-2xl">
+                        <label className="flex flex-col justify-center items-center text-2xl -mt-1">
                           10.6 Ξ{" "}
-                          <span className="text-sm font-normal text-grayDarkText tracking-wide">
+                          <span className="text-sm font-normal text-grayDarkText tracking-wide -mt-1">
                             $12,720 USD
                           </span>
                         </label>
@@ -168,7 +177,7 @@ const Dashboard = () => {
                     </div>
                   </div>
 
-                  <div className="w-full mx-8 my-4">
+                  <div className="w-full mx-8 my-2">
                     <div className=" flex flex-row justify-start items-center mb-1">
                       <h1 className="mr-10 font-bold tracking-wide text-2xl">
                         Summary Statistics{" "}
@@ -188,13 +197,31 @@ const Dashboard = () => {
                         </div>
                       </div>
                       <div className="w-1/3">
-                        <h1 className="text-base font-bold tracking-wide -mb-3">
+                        <h1 className="text-base font-bold tracking-wide ">
                           Royalty Share
                         </h1>
-                        <div className="h-32 w-52 mb-2">
-                          <DoughnutChart />
+                        <div className="flex flex-row h-28 w-full mb-0.5">
+                          <div className=" -mt-4 w-24">
+                            <DoughnutChart />
+                          </div>
+                          <div className="w-max flex flex-col flex-wrap justify-start items-start">
+                            {legend.labels.map((el, index) => {
+                              return (
+                                <div className="my-1 mx-3 flex flex-row ">
+                                  <div
+                                    className={``}
+                                    style={{
+                                      backgroundColor: `${legend.backgroundColor[index]}`,
+                                    }}
+                                  >
+                                    <h1 className="text-sm">{el}</h1>
+                                  </div>
+                                </div>
+                              );
+                            })}
+                          </div>
                         </div>
-                        <div className="h-32 w-52">
+                        <div className="-mt-4 w-24">
                           <DoughnutChart />
                         </div>
                       </div>
